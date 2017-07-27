f53d9b95e061e4205123d1968cda771b2a4dd58a
Jacob Tran <jacobtran@gmail.com>
2017-07-26 20:02:38 -0700

    add day 2 files

---
+++ day_2/review.js
@@ -0,0 +1,111 @@
+// good evening fundamentals!
+// this is new text
+
+// javascript - day 1
+
+// interpreted language -> javascript
+
+// compiled language -> c++
+// 1s and 0s
+
+// it was created in 10 days
+// ecmascript 6.0
+
+// javascript - java programming language
+
+// netscape & javascript
+
+// 2017
+
+
+1 + 1;
+4 * 2;
+
+// how can we run the 2 lines of js we just wrote?
+
+// 1.
+// through html we can execute javascript
+// <script>
+// </script>
+
+// 2. chrome, developer tools -> javascript console
+// alt + cmd + j
+
+// 3. a website that hosts a compiler. eg. repl.it, jsbin.com
+
+// 4. console, bash, terminal, command line -> node.js
+
+// variable - container for information, allocates some memory to store a value
+let a = 10;
+let b = 5;
+a * b; // 50
+
+// constant - a constant value that is assigned to a variable
+const pi = 3.14159;
+
+// var - variable
+// an older implementation, deprecated
+var c = "Jacob";
+var d = "Tran";
+c + d; // "JacobTran"
+
+// scope - the reach of a variable. see the difference between var and let.
+
+// loops
+// infinite loop
+while(true) {
+  // code block
+  console.log("hello");
+}
+
+// infinite loop
+let counter = 0;
+
+while(counter < 10) {
+  // code block
+  console.log("hello " + counter);
+  // incrementing the counter
+  counter++; // ++ operand
+
+  // counter = counter + 1;
+  // counter += 1;
+}
+
+// a shorter way to write a loop
+for (let counter = 0; counter < 10; counter++) {
+  console.log("hello " + counter);
+}
+
+// data structures
+// arrays
+// objects?
+
+
+
+// password for wifi is Community
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+//
