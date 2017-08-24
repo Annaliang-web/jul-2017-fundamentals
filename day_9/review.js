11ee4005ed52018c41f743c837edaa275cdf8b8a
Jacob Tran <jacobtran@gmail.com>
2017-08-23 21:04:59 -0700

    add day 9 files

---
+++ day_9/review.js
@@ -0,0 +1,16 @@
+// good evening fundies!
+
+// logical reasoning
+
+// jquery - a javascript library created by john resig. writing less code to do more.
+// allows us to tap into the DOM - document object model.  API for the webpage.
+
+// what is step 1 if we want to use jquery
+// $$$$ - MONEY
+// $ - selector ???
+
+// include jquery into our webpage
+// <script src="js/jquery-3.2.0.js"></script>
+
+// 2. do some exercise
+// to do list - demo
